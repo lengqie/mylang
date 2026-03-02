039e68a005492fc3db55b3af75a2e63ba7008a12
lengqie <lengqie@foxmail.com>
2026-03-02 20:48:25 +0800

    feat:add read_source function

--- main.c
+++ main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 static char* read_source(char* filename){
-    return 0;
+    FILE* f = fopen(filename, "rb");
+    if(f == NULL){
+        return NULL;
+    }
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    char* buffer = malloc(size + 1);
+    fread(buffer, 1, size, f);
+    buffer[size] = '\0';
+    fclose(f);
+    return buffer;
+
 }
 int main(int argc, char *argv[])
 {
     if(argc > 1){
         char* source = read_source(argv[1]);
+        printf("%s", source);
+    } else {
+        printf("Hello World");
     }
-    printf("Hello World");
     return 0;
 }
