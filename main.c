1d7d665d6d92e8b331a277eb144996d5579e6028
lengqie <lengqie@foxmail.com>
2026-03-06 14:51:04 +0800

    feat:add lex function

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum{
+    TOKEN_EOF,      // \0
+    TOKEN_LPAREN,   // (
+    TOKEN_RPAREN,   // )
+    TOKEN_STRING,   // string type
+    TOKEN_PRINT,    // print()
+    TOKEN_ERROR,    // error
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char* value;
+} Token;
 static char* read_source(char* filename){
     FILE* f = fopen(filename, "rb");
     if(f == NULL){
@@ -16,11 +29,47 @@ static char* read_source(char* filename){
     return buffer;
 
 }
+static int pos = 0;     //lexer position
+
+static Token token_gen(TokenType type, char* value){
+    return (Token){type, value};
+}
+static Token lex(char* source){
+    if (source[pos] == '\0'){
+        return token_gen(TOKEN_EOF, NULL);
+    }
+    if (source[pos] == 'p' && strnmp(source + pos, "rint", 4) == 0){
+        pos +=5;
+        return token_gen(TOKEN_PRINT, "print");
+    }
+    if (source[pos] == '('){
+        pos++;
+        return token_gen(TOKEN_LPAREN, "(");
+    }
+    if (source[pos] == ')'){
+        pos++;
+        return token_gen(TOKEN_RPAREN, ")");
+    }
+    if (source[pos] == '"'){
+        char* start = source + pos + 1;
+        while(source[pos] != '"'){
+            pos++;
+        }
+        if (source[pos] == '\0'){
+            return token_gen(TOKEN_ERROR, "unterminated string");
+        }
+        char* end = source + pos;
+        pos++;
+        return token_gen(TOKEN_STRING, strndup(start, end - start));
+    }
+    return token_gen(TOKEN_EOF, NULL);
+}
 int main(int argc, char *argv[])
 {
     if(argc > 1){
         char* source = read_source(argv[1]);
-        printf("%s", source);
+        // printf("%s", source);
+        lex(source);
     } else {
         printf("Hello World");
     }
